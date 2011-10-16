931cad38a2eaf222bbca44b20aade00cae494f74
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2011-10-16 11:36:52 +0100

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Galaman::Application.config.secret_token = 'e74af69776f9a2eecb5ae133ee001c42687288867ed3c5152cc87e2900be7ff0aa0d592abfba7e3c28e325f1b0f11232848bb6c24ce140f94cb56aafcaf4b593'
