7f9a98e6f4131ac91f6391c020dd5a50e5af6935
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2011-10-16 11:37:04 +0100

    defined constants for disciplines.

--- app/models/discipline.rb
+++ app/models/discipline.rb
@@ -1,2 +1,18 @@
 class Discipline < ActiveRecord::Base
+  has_many :events
+  has_many :competitions, :through => :events
+
+  DISTANCES = [50, 100, 200, 400]
+  COURSES = ["SC", "LC"]
+  STROKES = [
+    "Backstroke", 
+    "Breaststroke", 
+    "Butterfly", 
+    "Freestyle", 
+    "Ind Medley"
+  ]
+
+  def name
+    sprintf("%s [%s] %dm (%s)", self.stroke, self.gender, self.distance, self.course)
+  end
 end
