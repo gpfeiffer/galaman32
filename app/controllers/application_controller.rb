1cd1db56296048779fbc3374ca1d623e00192d17
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2011-10-21 21:51:19 +0100

    defined general helper and controller methods for times and disciplines.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # how to convert a hash with components mins, secs, centis into time
+  def time_from_msc(opts)
+    m = opts[:mins].to_i
+    s = opts[:secs].to_i
+    c = opts[:centis].to_i
+    100 * (60 * m + s) + c
+  end
+
+  # how to find the discipline, or create a new one
+  def find_discipline_id(opts)
+    #FIXME: Why do these have to be strings, and not keys:
+    keys = ['gender', 'distance', 'course', 'stroke']
+    subs = opts.reject { |k, v| not keys.include?(k) }
+    discipline = Discipline.where(subs).first || Discipline.create(subs)
+    return discipline.id
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def time_to_msc(time)
+    centis = time % 100
+    time /= 100
+    secs = time % 60
+    mins = time / 60
+    if mins > 0
+      sprintf('%d:%02d.%02d', mins, secs, centis)
+    else
+      sprintf('%d.%02d', secs, centis)
+    end
+  end
 end
