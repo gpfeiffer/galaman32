fe057689d30e5552560bc09070eb171db5739abc
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2011-10-22 15:54:50 +0100

    Show qualification times as (colored) table.

--- app/models/qualification.rb
+++ app/models/qualification.rb
@@ -1,3 +1,24 @@
 class Qualification < ActiveRecord::Base
   has_many :qualification_times
+  has_many :disciplines, :through => :qualification_times
+
+  def age_groups
+    qualification_times.each.map { |x| [x.age_min, x.age_max] }.sort.uniq
+  end
+
+  def filter_disciplines(gender, course)
+    disciplines.uniq.find_all { |x| x.gender == gender and x.course == course }
+  end
+
+  def filter_time(discipline, age_group)
+    qualification_times.select{ |x| x.discipline == discipline and [x.age_min, x.age_max] == age_group }.first
+  end
+
+  def courses
+    qualification_times.each.map { |x| x.course }.sort.uniq
+  end
+
+  def genders
+    qualification_times.each.map { |x| x.gender }.sort.uniq
+  end
 end
