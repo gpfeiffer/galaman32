533160878ab8893baf9e9365714754f0b02218c3
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2011-10-28 15:40:43 +0100

    order competitions by (reverse) date.

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,4 +1,5 @@
 class Competition < ActiveRecord::Base
+  default_scope :order => 'date'
   has_many :events, :dependent => :destroy
   has_many :disciplines, :through => :events
   has_many :registrations, :dependent => :destroy
