0c36da30c7ea966d9c7fe0629294f9ce80e4e6ba
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2011-11-13 16:10:45 +0000

    added 'Happy birthday' to home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
+
+    # what time is it
+    @time = Time.zone.now
+
+    # whose birthday is it
+    @birthdays = Swimmer.select { |swimmer| swimmer.birthday.day == @time.day and swimmer.birthday.month == @time.month }
   end
 
 end
