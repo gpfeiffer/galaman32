328af33c0d4f877049ab711627111674ae8546ac
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2011-11-14 19:03:34 +0000

    added a helper to compute best from a list of results.

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -1,2 +1,11 @@
 module ResultsHelper
+  def best_result(results)
+    best = nil
+    results.each do |result|
+      if not best or best.time == 0 or result.time < best.time
+        best = result
+      end
+    end
+    return best
+  end
 end
