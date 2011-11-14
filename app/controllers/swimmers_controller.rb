b04567796c333145a529f9aba4cb7a6d44cac6c7
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2011-11-14 20:36:02 +0000

    use .ord to convert char to ascii code.

--- app/controllers/swimmers_controller.rb
+++ app/controllers/swimmers_controller.rb
@@ -15,7 +15,7 @@ class SwimmersController < ApplicationController
   def show
     @swimmer = Swimmer.find(params[:id])
     @results = @swimmer.results.group_by { |result| result.discipline }
-    @rows = @results.keys.map { |x| { :distance => x.distance, :course => x.course } }.sort_by { |x| [x[:distance], -x[:course][0]] }.uniq 
+    @rows = @results.keys.map { |x| { :distance => x.distance, :course => x.course } }.sort_by { |x| [x[:distance], -x[:course][0].ord] }.uniq 
 
     respond_to do |format|
       format.html # show.html.erb
