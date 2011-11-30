ceda71f82a5369dac27ab55adcd3cb8fcbe48755
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2011-11-30 19:21:53 +0000

    minor repair on events: keep pos when editing, link Back to competition.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,7 +1,7 @@
 module EventsHelper
   MAX_NO_EVENTS = 50
 
-  def available_poss(competition)
-    (1..MAX_NO_EVENTS).to_a - competition.events.map{ |x| x.pos }
+  def available_poss(competition, event)
+    (1..MAX_NO_EVENTS).to_a - (competition.events.map { |x| x.pos } - [event.pos])
   end
 end
