b5efcc55b0b9342654e1738b80d38a29623a494c
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2011-12-02 21:52:23 +0000

    linked registrations to competitions

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -2,11 +2,10 @@ class Competition < ActiveRecord::Base
   default_scope :order => 'date'
   has_many :events, :dependent => :destroy
   has_many :disciplines, :through => :events
-  has_many :registrations, :dependent => :destroy
-  has_many :swimmers, :through => :registrations
   has_many :standards
   has_many :qualifications, :through => :standards
   has_many :invitations, :dependent => :destroy
+  has_many :registrations, :through => :invitations
   has_many :clubs, :through => :invitations
 
   validates :name, :date, :presence => true
--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,7 @@
 class Invitation < ActiveRecord::Base
   belongs_to :club
   belongs_to :competition
+  has_many :registrations
+
+  validates :club_id, :competition_id, :presence => true
 end
