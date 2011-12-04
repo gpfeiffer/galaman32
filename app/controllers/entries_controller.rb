dec4fa1224085e085f68d8921531ff0243867538
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2011-12-04 13:42:42 +0000

    redirect to invitation after creating entry.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -55,7 +55,7 @@ class EntriesController < ApplicationController
 
     respond_to do |format|
       if @entry.save
-        format.html { redirect_to(competition_path(:id => @entry.event.competition_id, :club_id => @entry.swimmer.club_id), :notice => 'Entry was successfully created.') }
+        format.html { redirect_to(invitation_path(:id => @entry.registration.invitation_id, :club_id => @entry.swimmer.club_id), :notice => 'Entry was successfully created.') }
         format.xml  { render :xml => @entry, :status => :created, :location => @entry }
       else
         format.html { render :action => "new" }
