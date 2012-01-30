a6be80fc5a1dd1d8294205539b70d31254e34936
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2012-01-30 14:51:41 +0000

    more tuning of pdf rendering.

--- app/controllers/heats_controller.rb
+++ app/controllers/heats_controller.rb
@@ -16,6 +16,15 @@ class HeatsController < ApplicationController
       format.text { 
         render :file => 'heats/index.text.erb'
       }
+      format.tex {
+        code = render_to_string
+        dir = File.join(Rails.root, 'tmp', 'latex')
+        tex = File.join(dir, 'heats.tex')
+        File.open(tex, 'w') { |io| io.write(code) }
+        system("pdflatex -output-directory #{dir} #{tex}")
+        pdf = File.join(dir, 'heats.pdf')
+        render :file => pdf, :layout => false, :content_type => "application/pdf"
+      }
     end
   end
 
