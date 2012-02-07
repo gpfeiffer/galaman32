df1de9dd7703cb256ec2cbc93e8b79c724cb9b19
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2012-02-07 10:32:26 +0000

    set all types to 'I'.

--- db/migrate/20120207101209_set_type_in_disciplines.rb
+++ db/migrate/20120207101209_set_type_in_disciplines.rb
@@ -1,7 +1,13 @@
 class SetTypeInDisciplines < ActiveRecord::Migration
   def self.up
+    Discipline.all.each do |discipline|
+      discipline.update_attribute(:type, Discipline::TYPES.first)
+    end
   end
 
   def self.down
+    Discipline.all.each do |discipline|
+      discipline.update_attribute(:type, nil)
+    end
   end
 end
