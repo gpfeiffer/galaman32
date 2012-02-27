8c028bf9f6f252339949ca9b467f9d98a71a7fc8
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2012-02-27 16:20:54 +0000

    add email to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120224201623) do
+ActiveRecord::Schema.define(:version => 20120227161915) do
 
   create_table "aims", :force => true do |t|
     t.integer  "swimmer_id"
@@ -158,6 +158,7 @@ ActiveRecord::Schema.define(:version => 20120224201623) do
     t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
 
 end
