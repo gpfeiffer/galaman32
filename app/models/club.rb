5b899abc36438bc8691c75f9077d5f301ffe6620
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2013-01-23 21:58:50 +0000

    sort clubs by name.

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,4 +1,6 @@
 class Club < ActiveRecord::Base
+  default_scope :order => :full_name
+
   has_many :swimmers, :dependent => :destroy
   has_many :invitations, :dependent => :destroy
   has_many :competitions, :through => :invitations
