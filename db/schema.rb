029aed0dce8d9b804e74c19486354f4f1d7fe597
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2013-02-19 10:17:26 +0000

    added name column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130218201654) do
+ActiveRecord::Schema.define(:version => 20130219101632) do
 
   create_table "aims", :force => true do |t|
     t.integer  "swimmer_id"
@@ -194,6 +194,7 @@ ActiveRecord::Schema.define(:version => 20130218201654) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
