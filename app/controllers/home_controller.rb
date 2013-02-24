ed01d2e81b151308e9bc3e1098b7ca610b9f9cfe
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2013-02-24 13:45:34 +0000

    improved layout.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,13 @@ class HomeController < ApplicationController
 
     # whose birthday is it
     @birthdays = Swimmer.select { |swimmer| swimmer.birthday.day == @time.day and swimmer.birthday.month == @time.month }
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.js  {
+        user_session[:edit] = (params[:edit] == "true") 
+      }
+    end
   end
 
 end
