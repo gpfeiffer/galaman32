c4e922c366c30ac560998ba3a625e6593b68a850
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2013-10-20 16:02:32 +0100

    :dependent => :destroy

--- app/models/qualification.rb
+++ app/models/qualification.rb
@@ -1,9 +1,9 @@
 class Qualification < ActiveRecord::Base
-  has_many :qualification_times
+  has_many :qualification_times, :dependent => :destroy
   has_many :disciplines, :through => :qualification_times
-  has_many :standards
+  has_many :standards, :dependent => :destroy
   has_many :competitions, :through => :standards
-  has_many :aims
+  has_many :aims, :dependent => :destroy
 
   validates :name, :presence => true, :uniqueness => true
 
