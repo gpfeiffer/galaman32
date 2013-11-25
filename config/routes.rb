14e3e0ec0b1ee11833a2b11d145ce8d324dcdf38
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2013-11-25 19:51:04 +0000

    compacted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,15 @@
 Galaman::Application.routes.draw do
-  resources :assignments
-  resources :roles
-
   devise_for :users
 
   scope "/admin" do
     resources :users
   end
 
-  resources :supports
-  resources :aims
-  resources :seats
-  resources :relays
-
-  get "admin" => 'admin#index'
-  get "heats" => 'heats#index'
-  get "lanes" => 'lanes#index'
-
-  resources :invitations
-  resources :standards
-  resources :results 
-
-  get "home/index"
-
-  resources :entries
+  resources :competitions, :invitations, :registrations
+  resources :clubs, :swimmers, :disciplines, :entries, :results 
+  resources :qualifications, :qualification_times, :standards
+  resources :roles, :assignments, :supports, :aims
+  resources :relays, :seats
 
   resources :events do
     member do
@@ -32,53 +18,11 @@ Galaman::Application.routes.draw do
     end
   end
 
-  resources :registrations
-  resources :competitions
-  resources :qualification_times
-  resources :qualifications
-  resources :disciplines
-  resources :swimmers
-  resources :clubs
+  get "admin" => 'admin#index'
+  get "heats" => 'heats#index'
+  get "lanes" => 'lanes#index'
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
+  get "home/index"
 
   # Sample resource route within a namespace:
   #   namespace :admin do
@@ -87,13 +31,5 @@ Galaman::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
   root :to => "home#index"
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
