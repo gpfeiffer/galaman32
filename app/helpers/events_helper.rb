22707c6a026203b8023e65f71a428bfc9fbe2a00
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2015-07-31 01:35:22 +0100

    defined gender_word
    now use it!

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -4,4 +4,12 @@ module EventsHelper
   def available_poss(competition, event)
     (1..MAX_NO_EVENTS).to_a - (competition.events.map { |x| x.pos } - [event.pos])
   end
+
+  def gender_word(gender)
+    {
+      'F' => "Girls",
+      'M' => "Boys",
+      'X' => "Mixed",
+    }[gender]
+  end
 end
