33f6e80a5aa2a0961bee57a140433435b89fe86c
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2015-08-01 10:15:40 +0100

    improved results helpers

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -1,32 +1,18 @@
 module ResultsHelper
+  def stage_word(stage)
+    { 
+      "P" => "Prelims",
+      "S" => "Semis",
+      "F" => "Finals",
+    }[stage]
+  end
+
   def ordinal_number(obj)
     obj.blank? ? "" : "#{obj}."     
   end
 
   def best_result(results)
-    best = nil
-    results.each do |result|
-      if result and result.time and result.time > 0 and (best == nil or best.time > result.time)
-        best = result
-      end
-    end
-    return best
-  end
-
-  # FIXME: obsolete!
-  # how to graph a list of results (of the same swimmer)
-  def graph_results(results)
-    swimmer = results.first.entry.swimmer
-    graph = []
-    results.each do |result|
-      if result.time and result.time > 0
-        graph << [(result.date - (swimmer.birthday+8.years)).to_i, result.time]
-      end
-    end
-    # HACK: a single point doesn't seem to get an x-value
-    if graph.count == 1
-      graph << [graph[0][0]+1, graph[0][1]]
-    end
-    return graph.sort_by { |x| x[0] }
+    result = results.min
+    return result if result.time > 0
   end
 end
