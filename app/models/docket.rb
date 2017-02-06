e693b21c69e91fbde396794a4886ecb26f9a66da
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2017-02-06 16:46:52 +0000

    cater for docket w/o entries

--- app/models/docket.rb
+++ app/models/docket.rb
@@ -34,6 +34,48 @@ class Docket < ActiveRecord::Base
   end
 
   # sdif
+  def to_d0 # to be used in case swimmer is registered but not entered
+    line = {
+      :mark => "D0",
+      :orgc => "8",
+      :gap0 => "%-8s" % "",
+      :name => "%-28s" % swimmer.last_first.encode("ISO-8859-1"),
+      :siid => "%-12s" % swimmer.number,
+      :atch => "A",
+      :citz => "%-3s" % "",
+      :dofb => "%8s" % swimmer.birthday.strftime("%m%d%Y"),
+      :agen => "%2d" % subject.age,
+      :ssex => "%1s" % subject.gender,
+      :esex => "%-1s" % "",
+      :dist => "%-4s" % "",
+      :stro => "%-1s" % "",
+      :evnt => "%-4s" % "",
+      :ages => "%-4s" % "",
+      :date => "%-8s" % "",
+      :tim0 => "%-8s" % "",
+      :crs0 => "%-1s" % "",
+      :tim1 => "%-8s" % "",
+      :crs1 => "%-1s" % "",
+      :tim2 => "%-8s" % "",
+      :crs2 => "%-1s" % "",
+      :tim3 => "%-8s" % "",
+      :crs3 => "%-1s" % "",
+      :hea1 => "%-2s" % "",
+      :lan1 => "%-2s" % "",
+      :hea2 => "%-2s" % "",
+      :lan2 => "%-2s" % "",
+      :plc1 => "%-3s" % "",
+      :plc2 => "%-3s" % "",
+      :pnts => "%-4s" % "",
+      :strd => "%-2s" % "",
+      :flgh => "%-1s" % "",
+      :gap1 => ("   0 ") + ("%-10s" % ""),
+    }
+    line = SDIF[line[:mark]][:keys].map { |key| line[key] }.join
+    line[-4, 4] = Format.checksum(line)
+    line
+  end
+
   def to_d3
     line = {
       :mark => "D3",
