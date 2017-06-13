4e08ecc41aa5e2f7938a1642ef51029c45d42282
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2017-06-13 15:43:17 +0100

    new spreadsheet version

--- app/models/club.rb
+++ app/models/club.rb
@@ -23,8 +23,27 @@ class Club < ActiveRecord::Base
     full_name
   end
 
-  # output spreadsheet of this year's best performances
+  # output spreadsheet of PBs
   def to_csv
+    results_by_swimmer = Hash[swimmers.map { |s| [s, s.results] } ]
+    year_results = results_by_swimmer.values.sum
+    disciplines = year_results.map(&:discipline).uniq.sort_by(&:course).sort_by(&:distance).sort_by(&:stroke)
+    headers = [ "Swimmer", "Swim Ireland ID" ] + disciplines.map(&:nickname_course)
+    CSV.generate(headers: true) do |csv|
+      csv << headers
+      results_by_swimmer.each do |swimmer, swimmer_results|
+        row = [swimmer.first_last, swimmer.number]
+        disciplines.each do |discipline|
+          discipline_results = swimmer_results.select { |x| x.discipline == discipline and x.time.present? }
+          row << (discipline_results.any? ? ResultsController.helpers.best_result(discipline_results).to_s : "")
+        end
+        csv << row
+      end
+    end
+  end
+  
+  # output spreadsheet of this year's best performances
+  def to_csv_old
     date = Date.parse("August") - 1.year
     year_results = results.select { |result| result.date > date }
     disciplines = year_results.map(&:discipline).uniq.sort_by(&:course).sort_by(&:distance).sort_by(&:stroke)
