c321af4dc65d16c31380ab6264c84d5701a3b9b6
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2018-02-27 13:01:54 +0000

    order qualifications by name

--- app/models/qualification.rb
+++ app/models/qualification.rb
@@ -1,10 +1,12 @@
 class Qualification < ActiveRecord::Base
-  has_many :qualification_times, :dependent => :destroy
-  has_many :disciplines, :through => :qualification_times
-  has_many :standards, :dependent => :destroy
-  has_many :competitions, :through => :standards
-  has_many :aims, :dependent => :destroy
+  default_scope order: :name
 
-  validates :name, :presence => true, :uniqueness => true
+  has_many :qualification_times, dependent: :destroy
+  has_many :disciplines, through: :qualification_times
+  has_many :standards, dependent: :destroy
+  has_many :competitions, through: :standards
+  has_many :aims, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
 
 end
